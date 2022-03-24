5e93b6c38071b465551bbc6de03f3e56e448d365
Samiat20 <samiat.olanrewaju20@gmail.com>
2022-03-24 10:31:03 +0100

    edited data

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -6,10 +6,10 @@ function Banner (){
         <div className="header">
           <img src={profilepicture} />
           <div className="intro">
-            <h1 className="first"> Hi, I am</h1> 
+            <h1 className="first"> Hello, I am</h1> 
             <h1 className="name"> Samiat</h1>
             <p> I am a frontend Engineer </p>
-            <p> HTML, Css, Javascript, React </p>
+            <p> HTML, CSS, Javascript, React etc. </p>
           </div>
         </div>
       </div>
